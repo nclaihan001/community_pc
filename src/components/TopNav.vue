f42e04ac5823eda9ad9b47249aff8f629cadb052
nclaihan <nclaihan@foxmail.com>
2023-11-28 20:58:20 +0200

    初始化工程

---
+++ src/components/TopNav.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+<template>
+  <div>导航栏</div>
+</template>
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import TopNav from '@/components/TopNav.vue'
+</script>
+
+<template>
+  <main>
+    <TopNav />
+    <div>claaa</div>
+  </main>
+</template>
